ffbc60d9f30b1bddca38cce4f13051a6a57a93e2
ShifaaKhalil98 <shifaakhalil50@gmail.com>
2023-04-17 22:56:54 +0300

    added the restaurants table

---
+++ GoResto-backend/GoResto-Laravel/database/migrations/2023_04_17_194921_create_restaurants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('manager_id')->constrained('users');
+            $table->string('logo');
+            $table->foreignId('menu_id')->constrained('menus');
+            $table->string('location');
+            $table->boolean('approved');
+            $table->integer('number_of_tables');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
