6252a504ab9a62a9a38249a95867cace1c554ff3
ShifaaKhalil98 <shifaakhalil50@gmail.com>
2023-05-07 18:31:06 +0300

    frontend/desktop: called addInquiry api

--- goresto-frontend/goresto-desktop-app/src/pages/inquiry.jsx
+++ goresto-frontend/goresto-desktop-app/src/pages/inquiry.jsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import "../App.css";
 import NavBar2 from "../components/navBar2";
 import LeftMenu from "../components/leftMenu";
-import Input from "../components/input";
 import styles from "../css/inquiry.module.css";
 import MyButton from "../components/button";
 
@@ -12,6 +11,7 @@ const Inquiry = () => {
   const navigate = useNavigate();
   const [inquiryContent, setInquiryContent] = useState("");
   const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
   const [token, setToken] = useState(localStorage.getItem("token"));
 
   const validateForm = () => {
@@ -23,49 +23,26 @@ const Inquiry = () => {
     return isValid;
   };
 
-  //   const handleInputChange = (event) => {
-  //     setError(event.target.value);
-  //     if (validateForm) {
-  //       setError("");
-  //     }
-  //   };
-
   const handleSubmit = (event) => {
     event.preventDefault();
     if (token) {
       if (validateForm()) {
-        const data = new FormData();
-        data.append("content", inquiryContent);
-
-        // axios
-        //   .post(`http://127.0.0.1:8000/api/addInquiry`, data, {
-        //     headers: {
-        //       Authorization: `Bearer ${token}`,
-        //       "Content-Type": "multipart/form-data",
-        //     },
-        //   })
-        //   .then((response) => {
-        //     navigate("/menu");
-        //     // localStorage.setItem("name", response.data.message.name);
-        //   })
-        //   .catch((error) => {
-        //     console.error(error);
-        //     if (
-        //       error.response &&
-        //       error.response.data &&
-        //       error.response.data.status === "failure" &&
-        //       error.response.data.message === "taken"
-        //     )
-        //       setError("Name is taken");
-        //     else if (
-        //       error.response &&
-        //       error.response.data &&
-        //       error.response.data.status === "failure" &&
-        //       error.response.data.message ===
-        //         "you already added a restaurant on this account"
-        //     )
-        //       setError("You already added a restaurant on this account");
-        //   });
+        const data = { content: inquiryContent };
+        axios
+          .post(`http://127.0.0.1:8000/api/inquiry`, data, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "multipart/form-data",
+            },
+          })
+          .then((response) => {
+            console.log(response.data);
+            setSuccess("Inquiry added successfully!");
+            setInquiryContent("");
+          })
+          .catch((error) => {
+            console.error(error);
+          });
       }
     } else navigate("/signin");
   };
@@ -78,14 +55,15 @@ const Inquiry = () => {
       <div className={`flex-column ${styles.sectionContainer}`}>
         <NavBar2 sectionName="Inquiries" className="hidden" />
         <div className={`flex-column ${styles.form}`}>
-          {error && <p className={styles.error}>{error}</p>}
           <textarea
             type="text"
             value={inquiryContent}
             placeholder="type here"
-            className={styles.input}
+            className={`smallsize ${styles.input}`}
             onChange={(e) => {
               setInquiryContent(e.target.value);
+              setError("");
+              setSuccess("");
             }}
           />
           <MyButton
@@ -93,6 +71,8 @@ const Inquiry = () => {
             label="Submit"
             onClick={(event) => handleSubmit(event)}
           />
+          {error && <p className={styles.error}>{error}</p>}
+          {success && <p className={styles.success}>{success}</p>}
         </div>
       </div>
     </div>
