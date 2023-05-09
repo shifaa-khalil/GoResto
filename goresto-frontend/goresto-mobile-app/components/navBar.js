a1f23e90f5e7074f34c8d92d36fdfdadcf0b7e93
ShifaaKhalil98 <shifaakhalil50@gmail.com>
2023-05-09 10:32:23 +0300

    frontend/mobile: setup the sign out method

--- goresto-frontend/goresto-mobile-app/components/navBar.js
+++ goresto-frontend/goresto-mobile-app/components/navBar.js
@@ -1,10 +1,27 @@
 import React, { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
 import { View, Text } from "react-native";
 import { StyleSheet } from "react-native";
 
 const NavBar = () => {
   const [name, setName] = useState("");
+  const [token, setToken] = useState("");
+  const navigation = useNavigation();
+
+  // async function getToken(key) {
+  //   try {
+  //     const value = await AsyncStorage.getItem(key);
+  //     // return value !== null ? JSON.parse(value) : null;
+  //     setToken(JSON.parse(value));
+  //   } catch (error) {
+  //     console.error("Error retrieving data:", error);
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   getToken("token");
+  // }, []);
 
   async function getName(key) {
     try {
@@ -15,6 +32,15 @@ const NavBar = () => {
       console.error("Error retrieving data:", error);
     }
   }
+
+  async function removeData(key) {
+    try {
+      await AsyncStorage.removeItem(key);
+    } catch (error) {
+      console.log("error", error);
+    }
+  }
+
   useEffect(() => {
     getName("name");
   }, []);
@@ -22,15 +48,24 @@ const NavBar = () => {
   return (
     <View style={[styles.navBar]}>
       <Text style={styles.userName}>Welcome, {name}!</Text>
+      <Text
+        style={styles.link}
+        onPress={() => {
+          removeData("name");
+          removeData("token");
+          navigation.navigate("Signin");
+        }}
+      >
+        Sign out
+      </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   navBar: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "flex-end",
+    alignItems: "flex-end",
+    // justifyContent: "flex-end",
     width: "100%",
     paddingHorizontal: 25,
     paddingVertical: 20,
@@ -38,6 +73,9 @@ const styles = StyleSheet.create({
   userName: {
     fontSize: 20,
     justifySelf: "flex-end",
+  },
+  link: {
+    textDecorationLine: "underline",
   },
 });
 
