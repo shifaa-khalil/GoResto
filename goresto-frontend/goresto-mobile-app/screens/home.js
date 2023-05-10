8cdaac54f1da7ab88344341bc12c31575ee91444
ShifaaKhalil98 <shifaakhalil50@gmail.com>
2023-05-10 10:59:36 +0300

    frontend/mobile: added the loading spinner in restaurants screen

--- goresto-frontend/goresto-mobile-app/screens/home.js
+++ goresto-frontend/goresto-mobile-app/screens/home.js
@@ -52,130 +52,126 @@ const Home = () => {
   //     if (images[i] % 3 == 0) property = "center";
   //     else property = "space-between";
   //   }
-  return (
-    <View>
-      <ActivityIndicator size="large" color="#d43325" />
-    </View>
-  );
-  // return (
-  //   <ScrollView ref={scrollViewRef} showsVerticalScrollIndicator={false}>
-  //     <View style={[styles.container]}>
-  //       <NavBar />
-  //       <Image source={GoPro} style={[styles.heading]} />
-  //       <NavCard
-  //         color={styles.yellow}
-  //         text="Check my reservations"
-  //         icon={Reservations}
-  //         onPress={() => navigation.navigate("Reservations")}
-  //       />
-  //       <NavCard
-  //         color={styles.red}
-  //         text="Go to chats"
-  //         icon={Chats}
-  //         onPress={() => console.log("pressed")}
-  //       />
-  //       <Text style={[styles.text]}>Check categories</Text>
-  //       <TouchableOpacity onPress={handleScroll}>
-  //         <Image source={DownArrow} style={[styles.downArrow]} />
-  //       </TouchableOpacity>
 
-  //       <View style={[styles.categories]}>
-  //         <CategoryCard
-  //           text="Chinese"
-  //           image={Chinese}
-  //           onPress={() =>
-  //             navigation.navigate("Restaurants", { cuisine: "chinese" })
-  //           }
-  //         />
-  //         <CategoryCard
-  //           text="Japanese"
-  //           image={Japanese}
-  //           onPress={() =>
-  //             navigation.navigate("Restaurants", { cuisine: "japanese" })
-  //           }
-  //         />
-  //         <CategoryCard
-  //           text="Italian"
-  //           image={Italian}
-  //           onPress={() =>
-  //             navigation.navigate("Restaurants", { cuisine: "italian" })
-  //           }
-  //         />
-  //         <CategoryCard
-  //           text="Lebanese"
-  //           image={Lebanese}
-  //           onPress={() =>
-  //             navigation.navigate("Restaurants", { cuisine: "lebanese" })
-  //           }
-  //         />
-  //         <CategoryCard
-  //           text="French"
-  //           image={French}
-  //           onPress={() =>
-  //             navigation.navigate("Restaurants", { cuisine: "french" })
-  //           }
-  //         />
-  //         <CategoryCard
-  //           text="American"
-  //           image={American}
-  //           onPress={() =>
-  //             navigation.navigate("Restaurants", { cuisine: "american" })
-  //           }
-  //         />
-  //         <CategoryCard
-  //           text="Mexican"
-  //           image={Mexican}
-  //           onPress={() =>
-  //             navigation.navigate("Restaurants", { cuisine: "mexican" })
-  //           }
-  //         />
-  //         <CategoryCard
-  //           text="Indian"
-  //           image={Indian}
-  //           onPress={() =>
-  //             navigation.navigate("Restaurants", { cuisine: "indian" })
-  //           }
-  //         />
-  //         <CategoryCard
-  //           text="Turkish"
-  //           image={Turkish}
-  //           onPress={() =>
-  //             navigation.navigate("Restaurants", { cuisine: "turkish" })
-  //           }
-  //         />
-  //         <CategoryCard
-  //           text="Spanish"
-  //           image={Spanish}
-  //           onPress={() =>
-  //             navigation.navigate("Restaurants", { cuisine: "spanish" })
-  //           }
-  //         />
-  //         <CategoryCard
-  //           text="Greek"
-  //           image={Greek}
-  //           onPress={() =>
-  //             navigation.navigate("Restaurants", { cuisine: "greek" })
-  //           }
-  //         />
-  //         <CategoryCard
-  //           text="Thai"
-  //           image={Thai}
-  //           onPress={() =>
-  //             navigation.navigate("Restaurants", { cuisine: "thai" })
-  //           }
-  //         />
-  //       </View>
-  //       <TouchableOpacity
-  //         style={styles.row}
-  //         onPress={() => navigation.navigate("Restaurants")}
-  //       >
-  //         <Icon name="chevron-back" size={24} color="#000" />
-  //         <Text style={[styles.discover]}>Discover restaurants</Text>
-  //         <Icon name="chevron-forward" size={24} color="#000" />
-  //       </TouchableOpacity>
-  //     </View>
-  //   </ScrollView>
-  // );
+  return (
+    <ScrollView ref={scrollViewRef} showsVerticalScrollIndicator={false}>
+      <View style={[styles.container]}>
+        <NavBar />
+        <Image source={GoPro} style={[styles.heading]} />
+        <NavCard
+          color={styles.yellow}
+          text="Check my reservations"
+          icon={Reservations}
+          onPress={() => navigation.navigate("Reservations")}
+        />
+        <NavCard
+          color={styles.red}
+          text="Go to chats"
+          icon={Chats}
+          onPress={() => console.log("pressed")}
+        />
+        <Text style={[styles.text]}>Check categories</Text>
+        <TouchableOpacity onPress={handleScroll}>
+          <Image source={DownArrow} style={[styles.downArrow]} />
+        </TouchableOpacity>
+
+        <View style={[styles.categories]}>
+          <CategoryCard
+            text="Chinese"
+            image={Chinese}
+            onPress={() =>
+              navigation.navigate("Restaurants", { cuisine: "chinese" })
+            }
+          />
+          <CategoryCard
+            text="Japanese"
+            image={Japanese}
+            onPress={() =>
+              navigation.navigate("Restaurants", { cuisine: "japanese" })
+            }
+          />
+          <CategoryCard
+            text="Italian"
+            image={Italian}
+            onPress={() =>
+              navigation.navigate("Restaurants", { cuisine: "italian" })
+            }
+          />
+          <CategoryCard
+            text="Lebanese"
+            image={Lebanese}
+            onPress={() =>
+              navigation.navigate("Restaurants", { cuisine: "lebanese" })
+            }
+          />
+          <CategoryCard
+            text="French"
+            image={French}
+            onPress={() =>
+              navigation.navigate("Restaurants", { cuisine: "french" })
+            }
+          />
+          <CategoryCard
+            text="American"
+            image={American}
+            onPress={() =>
+              navigation.navigate("Restaurants", { cuisine: "american" })
+            }
+          />
+          <CategoryCard
+            text="Mexican"
+            image={Mexican}
+            onPress={() =>
+              navigation.navigate("Restaurants", { cuisine: "mexican" })
+            }
+          />
+          <CategoryCard
+            text="Indian"
+            image={Indian}
+            onPress={() =>
+              navigation.navigate("Restaurants", { cuisine: "indian" })
+            }
+          />
+          <CategoryCard
+            text="Turkish"
+            image={Turkish}
+            onPress={() =>
+              navigation.navigate("Restaurants", { cuisine: "turkish" })
+            }
+          />
+          <CategoryCard
+            text="Spanish"
+            image={Spanish}
+            onPress={() =>
+              navigation.navigate("Restaurants", { cuisine: "spanish" })
+            }
+          />
+          <CategoryCard
+            text="Greek"
+            image={Greek}
+            onPress={() =>
+              navigation.navigate("Restaurants", { cuisine: "greek" })
+            }
+          />
+          <CategoryCard
+            text="Thai"
+            image={Thai}
+            onPress={() =>
+              navigation.navigate("Restaurants", { cuisine: "thai" })
+            }
+          />
+        </View>
+        <TouchableOpacity
+          style={styles.row}
+          onPress={() => navigation.navigate("Restaurants")}
+        >
+          <Icon name="chevron-back" size={24} color="#000" />
+          <Text style={[styles.discover]}>Discover restaurants</Text>
+          <Icon name="chevron-forward" size={24} color="#000" />
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
--- goresto-frontend/goresto-mobile-app/screens/restaurants.js
+++ goresto-frontend/goresto-mobile-app/screens/restaurants.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
-import { ScrollView, View } from "react-native";
+import { ActivityIndicator, ScrollView, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { StyleSheet } from "react-native";
 import NavBar2 from "../components/navBar2";
@@ -23,6 +23,7 @@ const Restaurants = ({ route }) => {
   const [token, setToken] = useState("");
   const [params, setParams] = useState("");
   const [justEntered, setJustEntered] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   async function getData(key) {
     try {
@@ -47,6 +48,7 @@ const Restaurants = ({ route }) => {
       .then((response) => {
         console.log(response.data.restaurants);
         setRestaurants(response.data.restaurants);
+        setIsLoading(false);
       })
       .catch((error) => {
         console.log(error);
@@ -100,57 +102,73 @@ const Restaurants = ({ route }) => {
   };
 
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      style={[styles.screenContainer]}
+    >
       <View style={[styles.container]}>
-        <CategoryBar onCategorySelected={handleCategorySelection} />
-        <NavBar2
-          onChangeText={(text) => {
-            setSearchInput(text);
-          }}
-          onFilterSelected={(filter) => setSelectedFilter(filter)}
-          onMinPriceChange={(minPrice) => setMinPriceSelected(minPrice)}
-          onMaxPriceChange={(maxPrice) => setMaxPriceSelected(maxPrice)}
-          onMinRatingChange={(minRating) => setMinRatingSelected(minRating)}
-          onMaxRatingChange={(maxRating) => setMaxRatingSelected(maxRating)}
-          onLocationChange={(location) => setLocationSelected(location)}
-          onSubmit={handleSubmit}
-        />
-        <View style={[styles.restaurants]}>
-          {restaurants.map((restaurant) => (
-            <RestaurantCard
-              key={restaurant.id}
-              image={restaurant.logo}
-              name={restaurant.name}
-              rating={restaurant.rating}
-              // deposit={restaurant.deposit}
-              location={restaurant.location}
-              onPress={() =>
-                navigation.navigate("Restaurant", {
-                  restaurant_id: restaurant.id,
-                  name: restaurant.name,
-                  rating: restaurant.rating,
-                  location: restaurant.location,
-                  deposit: restaurant.deposit,
-                  image: restaurant.logo,
-                })
-              }
+        {isLoading ? (
+          <View style={styles.spinner}>
+            <ActivityIndicator size="large" color="#d43325" />
+          </View>
+        ) : (
+          <>
+            <CategoryBar onCategorySelected={handleCategorySelection} />
+            <NavBar2
+              onChangeText={(text) => {
+                setSearchInput(text);
+              }}
+              onFilterSelected={(filter) => setSelectedFilter(filter)}
+              onMinPriceChange={(minPrice) => setMinPriceSelected(minPrice)}
+              onMaxPriceChange={(maxPrice) => setMaxPriceSelected(maxPrice)}
+              onMinRatingChange={(minRating) => setMinRatingSelected(minRating)}
+              onMaxRatingChange={(maxRating) => setMaxRatingSelected(maxRating)}
+              onLocationChange={(location) => setLocationSelected(location)}
+              onSubmit={handleSubmit}
             />
-          ))}
-        </View>
+            <View style={[styles.restaurants]}>
+              {restaurants.map((restaurant) => (
+                <RestaurantCard
+                  key={restaurant.id}
+                  image={restaurant.logo}
+                  name={restaurant.name}
+                  rating={restaurant.rating}
+                  // deposit={restaurant.deposit}
+                  location={restaurant.location}
+                  onPress={() =>
+                    navigation.navigate("Restaurant", {
+                      restaurant_id: restaurant.id,
+                      name: restaurant.name,
+                      rating: restaurant.rating,
+                      location: restaurant.location,
+                      deposit: restaurant.deposit,
+                      image: restaurant.logo,
+                    })
+                  }
+                />
+              ))}
+            </View>
+          </>
+        )}
       </View>
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  screenContainer: {
     flex: 1,
     backgroundColor: "white",
+  },
+  container: {
     alignItems: "center",
   },
   restaurants: {
     width: 310,
     marginVertical: 40,
+  },
+  spinner: {
+    marginTop: 150,
   },
 });
 
