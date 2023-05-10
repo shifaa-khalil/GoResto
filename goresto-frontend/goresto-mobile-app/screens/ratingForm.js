ffc38d8fbda652e51c45d09099164456de3c5d88
ShifaaKhalil98 <shifaakhalil50@gmail.com>
2023-05-10 12:25:50 +0300

    frontend/mobile: converted the rating to use stars

--- goresto-frontend/goresto-mobile-app/screens/ratingForm.js
+++ goresto-frontend/goresto-mobile-app/screens/ratingForm.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { StyleSheet } from "react-native";
 import Input from "../components/input";
 import MyButton from "../components/button";
+import EmptyStar from "../assets/emptyStar.png";
 import { URL } from "../configs/URL";
 
 const Rating = ({ route }) => {
@@ -14,6 +15,7 @@ const Rating = ({ route }) => {
   const [content, setContent] = useState("");
   const [error, setError] = useState("");
   const [token, setToken] = useState("");
+  const [starPressed, setStarPressed] = useState(null);
 
   async function getData(key) {
     try {
@@ -61,12 +63,20 @@ const Rating = ({ route }) => {
     }
   };
 
+  useEffect(() => {
+    if (starPressed == 1) setRating(1);
+    else if (starPressed == 2) setRating(2);
+    else if (starPressed == 3) setRating(3);
+    else if (starPressed == 4) setRating(4);
+    else if (starPressed == 5) setRating(5);
+  }, [starPressed]);
+
   return (
     <View style={[styles.container]}>
       <Text>{error}</Text>
       <View style={[styles.form]}>
         <Text style={styles.restoName}>{route.params.name}</Text>
-        <Input
+        {/* <Input
           title="Rating"
           placeHolder="Rating"
           value={rating}
@@ -74,7 +84,25 @@ const Rating = ({ route }) => {
             setRating(text);
             handleChangeText();
           }}
-        />
+        /> */}
+        <Text style={styles.label}>Rating</Text>
+        <View style={styles.stars}>
+          <TouchableOpacity onPress={() => setStarPressed(1)}>
+            <Image source={EmptyStar} style={styles.emptyStar} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setStarPressed(2)}>
+            <Image source={EmptyStar} style={styles.emptyStar} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setStarPressed(3)}>
+            <Image source={EmptyStar} style={styles.emptyStar} />
+          </TouchableOpacity>{" "}
+          <TouchableOpacity onPress={() => setStarPressed(4)}>
+            <Image source={EmptyStar} style={styles.emptyStar} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setStarPressed(5)}>
+            <Image source={EmptyStar} style={styles.emptyStar} />
+          </TouchableOpacity>{" "}
+        </View>
         <Input
           title="Content"
           placeHolder="Content"
@@ -113,6 +141,17 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginVertical: 30,
     fontSize: 25,
+  },
+  stars: {
+    flexDirection: "row",
+    columnGap: 10,
+    marginBottom: 20,
+    marginTop: 5,
+  },
+  label: { fontSize: 20 },
+  emptyStar: {
+    height: 20,
+    width: 20,
   },
 });
 
